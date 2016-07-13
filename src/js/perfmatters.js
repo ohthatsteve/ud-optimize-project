992e2caa4bbd03de3a459db5d135729dfd249e82
Steve Cooper <stevencoop89@outlook.com>
2016-07-13 19:54:53 -0400

    move and defer non critical javascript

--- src/js/perfmatters.js
+++ src/js/perfmatters.js
@@ -1,4 +1,4 @@
-// Measuring the Critical Rendering Path with Navigation Timing
+// Measuring the Critical Rendering Path With Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
 function logCRP() {
