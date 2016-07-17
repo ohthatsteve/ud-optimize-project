bd05aede11c95387a0f8dba19d5369198dff1900
Steve Cooper <stevencoop89@outlook.com>
2016-07-17 17:39:44 -0400

    remove unused variable

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -543,7 +543,6 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  var pizza = document.querySelector("#movingPizzas1");
   for (var i = 0; i < 200; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
